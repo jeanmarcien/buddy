2f0fec6e75619e3b0f7f894ccf0f347f160b0174
Jean-Marcien Kablansi <jean.marcien@gmail.com>
2024-11-22 23:04:02 -0500

    Update the user pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#index"
-  
+
   get 'pages/about', as: 'about'
   get 'pages/how', as: 'how'
-  
+
   # Route pour les erreurs 404
   match '/404', to: 'errors#not_found', via: :all
-  
+
   # Rediriger toutes les autres routes non trouvées vers la page 404
   match '*path', to: 'errors#not_found', via: :all
 end
