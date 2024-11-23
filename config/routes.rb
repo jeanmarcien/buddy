dc2c8e5c0264699fe54ca45dc32ed5425d6d96a3
FannyEve PotvinLemay <fannyevepl@gmail.com>
2024-11-23 17:14:38 -0500

    rollback

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  
-  authenticated :user do
-    root to: 'dashboard#index', as: :authenticated_root
-  end
+  devise_for :users
   root to: "pages#index"
   get 'pages/about', as: 'about'
   get 'pages/how', as: 'how'
@@ -29,11 +24,10 @@ Rails.application.routes.draw do
     resources :vet_appointments
   end
 
+
   # Route pour les erreurs 404
   match '/404', to: 'errors#not_found', via: :all
 
   # Rediriger toutes les autres routes non trouvées vers la page 404
   match '*path', to: 'errors#not_found', via: :all
-
-  get 'dashboard', to: 'dashboard#index', as: 'dashboard'
 end
