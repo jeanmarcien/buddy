44ed47e0c080718f01aa909e016abd8519a8c9d1
FannyEve PotvinLemay <fannyevepl@gmail.com>
2024-11-26 21:01:52 -0500

    ajout des pet bouton

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_23_182247) do
     t.date "birth_day"
     t.string "gender"
     t.bigint "user_id", null: false
-    t.bigint "vet_id", null: false
+    t.bigint "vet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_pets_on_user_id"
