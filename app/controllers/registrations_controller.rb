17e2ecc234a5c8750e3a53c88045907553db4130
Jean-Marcien Kablansi <jean.marcien@gmail.com>
2024-11-30 15:50:05 -0500

    redirect the signup page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    new_pet_path(resource)
+    pets_path(resource)
   end
 end
