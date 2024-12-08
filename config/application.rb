b5c75accc03b2970b1e73e6379540bc2febed76c
santiago velazquez <santiagovdiaz@outlook.com>
2024-12-07 21:34:31 -0500

    video functionality

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module Buddy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.session_store :cookie_store, key: '_buddy_session'
   end
 end
