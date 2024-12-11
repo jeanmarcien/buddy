24264b635379c9080cefec8fca2382fddd9d9c81
Erwan Peres <eperes@jebidouille.com>
2024-12-11 14:11:59 -0500

    logo scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_11_013655) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_11_020902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
